c41de6fb31bc56162b4747456a373bbbae3a7dbd
TOPTH0N <119265616+TOPTH0N@users.noreply.github.com>
2023-07-21 14:42:29 -0700

    Update sample_config.py

--- sample_config.py
+++ sample_config.py
@@ -30,7 +30,7 @@ class Config(object):
     TZ = os.environ.get("TZ", "Asia/Baghdad")
     # set this with required zed repo link
     UPSTREAM_REPO = os.environ.get(
-        "UPSTREAM_REPO", "https://github.com/TOPTH0N/ALSiD12"
+        "UPSTREAM_REPO", "https://github.com/TOPTH0N/ALSiD"
     )
     # External plugins repo
     EXTERNAL_REPO = os.environ.get("EXTERNAL_REPO", None)
